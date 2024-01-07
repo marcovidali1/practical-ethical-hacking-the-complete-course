0915dc51113f753938f1b4e5f3be9c114331e6bb
--global <--global>
2024-01-07 17:02:40 +0100

    Math

---
+++ Introduction to Python/math.py
@@ -0,0 +1,12 @@
+#!/bin/python3
+
+# math
+print(50 + 50)  # add
+print(50 - 50)  # subtract
+print(50 * 50)  # multiply
+print(50 / 50)  # divide
+print(50 + 50 - 50 * 50 / 50)  # pedmas
+print(50 ** 50)  # exponents
+print(50 % 6)  # modulo
+print(50 / 6)
+print(50 // 6)  # no leftovers
