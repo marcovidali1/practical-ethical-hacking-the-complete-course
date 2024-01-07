c3a72c76f6a4b6c5a3065cd9c6ab699937bc9cef
--global <--global>
2024-01-07 17:08:49 +0100

    Variables and Methods

---
+++ Introduction to Python/script.py
@@ -0,0 +1,24 @@
+#!/bin/python3
+
+# variables and methods
+quote = "Rem tene verba sequentur."
+print(quote.upper())  # uppercase
+print(quote.lower())  # lowercase
+print(quote.title())  # title case
+
+print(len(quote))
+
+name = "Marco"  # string
+age = 16  # int int(16)
+gpa = 3.7  # float float(3.7)
+
+print(int(age))
+print(int(16.9))
+
+print("My name is " + name + " and I am " + str(age) + " years old.")
+age += 1
+print(age)
+
+birthday = 1
+age += birthday
+print(age)
