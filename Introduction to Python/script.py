2628ab77291264e567bfbf60f3ef560494a9cde4
--global <--global>
2024-01-08 17:22:41 +0100

    Boolean Expressions

--- Introduction to Python/script.py
+++ Introduction to Python/script.py
@@ -74,3 +74,13 @@ def nl():
 
 
 nl()
+
+# boolean expressions (True or False)
+print("Boolean expressions:")
+
+bool1 = True
+bool2 = 3 * 3 == 9
+bool3 = False
+bool4 = 3 * 3 != 9
+print(bool1, bool2, bool3, bool4)
+print(type(bool1))
