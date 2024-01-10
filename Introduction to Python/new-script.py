d37829511cfee004d54e22bf0b965c77d5bd7ace
--global <--global>
2024-01-10 17:36:13 +0100

    Dictionaries

--- Introduction to Python/new-script.py
+++ Introduction to Python/new-script.py
@@ -31,3 +31,23 @@ print(letter.lower() in word.lower())
 
 movie = "The Hangover"
 print("My favorite movie is {}.".format(movie))
+
+# dictionaries - key/value pairs {}
+drinks = {"White Russian": 7, "Old Fashion": 10,
+          "Lemon Drop": 8}  # drink is key, price is value
+print(drinks)
+
+employees = {"Finance": ["Bob", "Linda", "Tina"], "IT": [
+    "Gene", "Louise", "Teddy"], "HR": ["Jimmy", "Jr.", "Mort"]}
+print(employees)
+
+employees["Legal"] = ["Mr. Frond"]  # add new key:value pair
+print(employees)
+
+employees.update({"Sales": ["Andie", "Ollie"]})
+print(employees)
+
+drinks["White Russian"] = 8
+print(drinks)
+
+print(drinks.get("White Russian"))
